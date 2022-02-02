1a6554652c6889edcb6760e22b4dd70ebba49b07
vlasnata80@gmail.com <vlasnata80@gmail.com>
2022-02-02 12:02:11 +0200

    Add store

---
+++ frontend/animal-frontend/src/store/action-creators/index.ts
@@ -0,0 +1,11 @@
+//import * as UserActions from '../../components/authorize/Login/actions';
+//import * as RegisterActions from '../../components/authorize/Register/actions';
+//import * as ProductFetchActions from '../../components/products/actions';
+
+const actions = {
+    //...UserActions,
+    //...RegisterActions,
+   // ...ProductFetchActions
+}
+
+export default actions;
